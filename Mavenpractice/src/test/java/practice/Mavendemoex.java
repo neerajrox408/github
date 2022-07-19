f648cb3ce1e5a228db4d186f517265088ef5d909
admin <admin@DESKTOP-G6DLKAJ>
2022-07-19 11:43:50 +0530

    first github project

---
+++ Mavenpractice/src/test/java/practice/Mavendemoex.java
@@ -0,0 +1,39 @@
+package practice;
+import org.testng.annotations.Test;
+
+public class Mavendemoex {
+	@Test
+	public void mavendemoextest()
+	{
+		System.out.println("hello");
+	}
+	
+@Test
+public void mavendemoextest1()
+
+	{
+		System.out.println("bye");
+	}
+@Test
+public void mavendemoextest2()
+{
+	System.out.println("hhhye");
+}
+//mvn test -Dtest=testngclassname
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Mavenpractice/src/test/java/practice/Mavendemoex2.java
@@ -0,0 +1,21 @@
+package practice;
+import org.testng.annotations.Test;
+
+public class Mavendemoex2 {
+@Test
+public void mavenpractice()
+{
+	System.out.println("project launched");
+}
+@Test
+public void mavenpractice1()
+{
+	System.out.println("project complete");
+}
+
+
+
+
+
+
+}
